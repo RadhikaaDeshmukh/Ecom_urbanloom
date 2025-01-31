8318303ed20c767953db445c9cb7988edde44829
RadhikaaDeshmukh <radikaadeshmukh29@gmail.com>
2025-01-31 18:08:24 +0530

    Updated code with new components

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -10,26 +10,23 @@ const AboutUs = () => {
 <span
     style={{ position: "absolute",bottom: "-20px",left: "50%", transform: "translateX(-50%)", width: "60px", height: "2px", backgroundColor: "skyblue", display: "block", }}/>
 </h1>
-<br /><br />
-<div style={{ display: "flex",justifyContent: "center", alignItems: "center", backgroundColor: "#f9f9f9"}}>
-  <div style={{width: "1000px",padding: "20px",lineHeight: "1.8",fontSize: "16px",color: "#333",textAlign: "justify" }}>
+<br />
+<div className="mainContainer">
+  <div className="aboutmain">
     Today, growing concern over climate change is altering the way we historically thought about lifestyle and leisure.
     With awareness of the negative impact of synthetic fabrics increasing, consumers are searching for ways to reduce
     their environmental footprint and are eager to adopt natural fibers. However, there are still barriers to entry,
     including availability and variety, which leaves consumers with limited choices.
-    <br />
     <br />
     Started in 2018, Urbanloom is a homegrown brand that brings high-quality, naturally sourced home furnishing products
     for the Indian consumer. With its foundation in sustainability, Urbanloom makes conscious consumerism stylish and
     functional for everyone. As an inclusive brand, we use both traditional and modern techniques to create unique
     products that are built to last.
     <br />
-    <br />
     We have an in-house production unit where we design eco-friendly, skin-friendly, comfortable, stylish, colorful,
     and luxurious ranges of cotton, jute, and denim bean bags, poufs, ottomans, and other home furnishing products.
     Urbanloom is emerging as a prominent eco-conscious brand, which is all set to revamp the home and lifestyle space
     with sustainable alternatives.
-    <br />
     <br />
     We supply bean bags to tech start-ups, urban cafes, and spas. For bulk and corporate inquiries, drop an email to{" "}
     <a href="mailto:support@urbanloom.in" style={{ color: "#007BFF" }}>
@@ -38,7 +35,8 @@ const AboutUs = () => {
   </div>
 </div>
 <br />
-
+<br />
+<br />
 <Footer />
     </>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,23 +2,46 @@ import React from 'react'
 import firstcar from '../assets/happynew.jpg'
 import secondcar  from '../assets/landscapehandcrafted.jpg'
 import thirdcar from '../assets/yellow_multiple.jpg'
+import './Home.css'
+import mindLogo from '../assets/mind.jpg'
+import handcraftedLogo from '../assets/handcrafted.jpg'
+import artisanal from '../assets/artisanal.jpg'
+import sustainable from '../assets/sustainable.jpg'
+import firstcard from '../assets/Card/classic1.jpg'
+import secondcard from '../assets/Card/football2.jpg'
+import thirdcard from '../assets/Card/comboclas3.jpg'
+import fourtcard from '../assets/Card/chair_bean_.jpg'
+import fifthcard from '../assets/Card/footballc.jpg'
+import sixthcard from '../assets/Card/poufs.jpg'
+import trendingCard from '../assets/binbag.jpg'
+import trendingCard1 from '../assets/binbag2.jpg'
+import { Input } from '@mui/material'
 
 const Home = () => {
   return (
     <>
-    <h1>Header </h1>
+    <div className="container-fluid" style={{marginTop:'40px'}}>
+  <div className="row justify-content-center">
+    <div className="col-10"  > 
     <div id="carouselExample" className="carousel slide">
   <div className="carousel-inner">
   <div className="carousel-item active">
-            <img src={firstcar}  className="d-block w-100" alt="Slide 1" />
+            <img src={firstcar}  className="d-block w-100" alt="Slide 1" height={450} />
+            <div className="carousel-caption d-none d-md-block">
+              <h6>UNMATCHED COMFORT</h6>
+              <h1 style={{marginBottom:'140px'}}>Unbelievably good looks</h1>
+            </div>
      </div>
     <div className="carousel-item">
-             <img src={secondcar}  className="d-block w-100" alt="..."/>
+             <img src={secondcar}  className="d-block w-100" alt="..." height={400}/>
     </div>
     <div className="carousel-item">
-      <img src={thirdcar}  className="d-block w-100" alt="..." />
+      <img src={thirdcar}  className="d-block w-100" alt="..." height={400} />
     </div>
   </div>
+  </div>
+  </div>
+</div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
@@ -26,7 +49,118 @@ const Home = () => {
   <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
     <span className="carousel-control-next-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
-  </button>
+    </button>
+
+
+ <div className='textContainer'>
+<p>Urbanloom products are carefully hand crafted to perfection from pure cotton fabric made in traditional looms. Now buy bean bags, ottomans & cushions  in host of vibrant colors and designs and spice up your living room, bed room or study. Our products are skin-friendly, eco-friendly , breathable and fun!</p>
+    </div>
+ 
+
+<div className='circleLogo'>
+  <img className="imgcircleLogo" src={mindLogo} alt="" />
+  <img className="imgcircleLogo" src={sustainable} alt="" />
+  <img className="imgcircleLogo" src={handcraftedLogo} alt="" />
+  <img className="imgcircleLogo" src={artisanal} alt="" />
+</div>
+
+<div className='homeCardmain'>
+  <div>
+  <h3>Cotton Bean Bags
+  <span
+    style={{ position: "absolute",bottom: "-10px",left: "50%", transform: "translateX(-50%)", width: "60px", height: "2px", backgroundColor: "skyblue", display: "block", }}/>
+  </h3>
+  </div>
+  <div className="card-container">
+    <div className="imgdiv">
+        <img src={firstcard} alt="" className="cardImg" />
+        <div className="overlay"></div>  {/* Dark overlay */}
+        <div className="cardText">Classic Teardrops</div>
+    </div>
+    <div className="imgdiv">
+        <img src={secondcard} alt="" className="cardImg" />
+        <div className="overlay"></div>
+        <div className="cardText">Footballs</div>
+    </div>
+    <div className="imgdiv">
+        <img src={thirdcard} alt="" className="cardImg" />
+        <div className="overlay"></div>
+        <div className="cardText">Classic Combos</div>
+    </div>
+    <div className="imgdiv">
+        <img src={fourtcard} alt="" className="cardImg" />
+        <div className="overlay"></div>
+        <div className="cardText">Loungers</div>
+    </div>
+    <div className="imgdiv">
+        <img src={fifthcard} alt="" className="cardImg" />
+        <div className="overlay"></div>
+        <div className="cardText">Football Combos</div>
+    </div>
+    <div className="imgdiv">
+        <img src={sixthcard} alt="" className="cardImg" />
+        <div className="overlay"></div>
+        <div className="cardText">Poufs</div>
+    </div>
+</div>
+
+
+
+</div>
+
+<div className='homeCardmain'>
+  <div>
+  <h3>Trending Products
+  <span
+    style={{position: "absolute",bottom: "-10px",left: "50%", transform: "translateX(-50%)", width: "60px", height: "2px", backgroundColor: "skyblue", display: "block", }}/>
+  </h3>
+  </div>
+
+<div className='trendingProducts'>
+    <div className="trendingCard">
+      <img src={trendingCard1} alt='trendingCard' className='imgtrending'/>
+      <p className="paraTrending">Auburn cotton handloom bean bag Cover without beans<br/>
+         <b className="oldPrice">Rs. 2,999.00</b> Rs. 2,199.00</p>
+    </div>
+    <div className="trendingCard">
+    <img src={trendingCard1} alt='trendingCard'  className='imgtrending'/>
+    <p className="paraTrending">Acacia cotton handloom bean bag Cover without beans<br/>
+    <b className="oldPrice">Rs. 2,999.00</b> Rs. 2,199.00</p>
+    </div>
+    <div className="trendingCard">
+    <img src={trendingCard1} alt='trendingCard' className='imgtrending' />
+    <p className="paraTrending">Kiera XXXL cotton handloom bean bag lounger without beans (Red)<br/>
+    <b className="oldPrice">Rs. 2,999.00</b> Rs. 2,299.00</p>
+    </div>
+    <div className="trendingCard">
+    <img src={trendingCard1} alt='trendingCard' className='imgtrending' />
+    <p className="paraTrending">Cotton Handloom Moroccan Pouf cover , Blue & Red<br/>
+    <b className="oldPrice">Rs. 2,999.00</b> Rs. 999.00</p>
+    </div>
+    <div className="trendingCard">
+    <img src={trendingCard1} alt='trendingCard' className='imgtrending' />
+    <p className="paraTrending">Scarlet cotton handloom bean bag cover & Footstool cover<br/>
+    <b className="oldPrice">Rs. 2,999.00</b> Rs. 2,999.00</p>
+    </div>
+    <div className="trendingCard">
+    <img src={trendingCard1} alt='trendingCard' className='imgtrending' />
+    <p className="paraTrending">Bumblebee cotton handloom Football bean bag Cover & Footstool cover<br/>
+    <b className="oldPrice">Rs. 2,999.00</b> Rs. 2,999.00</p>
+    </div>
+</div>
+
+<div className='subscribesec'>
+   <h2>Subscribe to our newsletter</h2>
+   <h4>Promotions, new products and sales. Directly to your inbox.
+   <span
+    style={{position: "absolute",bottom: "-10px",left: "50%", transform: "translateX(-50%)", width: "60px", height: "2px", backgroundColor: "skyblue", display: "block", }}/>
+   </h4>
+
+    <Input className='subscribeemail' type='text' placeholder='Your email' /><button type='submit'>SUBSCRIBE</button>
+</div>
+
+
+</div>
 </div>
     
     
